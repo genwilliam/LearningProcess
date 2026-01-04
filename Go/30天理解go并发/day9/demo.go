71076f38f134789c8c4b4ed2782768a9cc474c6e
william <genwillen@gmail.com>
2026-01-04 15:07:05 +0800

    docs: add golang file

--- Go/30天理解go并发/day9/demo.go
+++ Go/30天理解go并发/day9/demo.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 // Fan-out mod
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+	ch := make(chan int, 2)
 
+	go func() {
+		defer close(ch)
+		for i := 1; i <= 20; i++ {
+			ch <- i
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		go func(id int) {
+			defer wg.Done()
+
+			// range 直接作用于 channel
+			for task := range ch {
+				fmt.Printf("Worker %d 处理了任务: %d\n", id, task)
+			}
+		}(i)
+	}
+
+	wg.Wait()
 }
